0f3ab13f8b1dfa30768aa0d440b0ba7aca193ea5
Aaron Ortiz <aronortiz90@yahoo.com>
2020-10-15 17:50:34 -0500

    add clase number and it's test

--- src/main/java/nombre.java
+++
@@ -1,5 +0,0 @@
-public class nombre {
-    String sMiCadena = "En un lugar de la Mancha";
-System.out.println("Cadena de Texto: '" + sMiCadena + "'");
-System.out.println("Tamaño de la cadena: " +   sMiCadena.length() + " caracteres");
-}
---
+++ src/test/java/com/uni/edu/ec/examen/NumberTest.java
@@ -0,0 +1,7 @@
+package com.uni.edu.ec.examen;
+
+import static org.junit.Assert.*;
+
+public class NumberTest {
+
+}
